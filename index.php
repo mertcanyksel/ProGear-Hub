7b38063b0548461270c27af2b8ee938258414d2d
Mertcan <64431136+mertcanyksel@users.noreply.github.com>
2025-08-14 09:07:10 +0000

    card-content was wrong name

--- index.php
+++ index.php
@@ -1,88 +1,108 @@
 <!DOCTYPE html>
 <html lang="en">
-    <?php include "includes/head.php"; ?>    <!---28.07.2025-->
-    <body>
-        
-        <?php include "includes/pageheader.php"; ?> 
-        <div class="home-banner">
-            <div class="banner-text">
-                <h2>SALE</h2>
-                <P>Get massive discounts in our mid-year sale</P>
-                <a href="sale.html">Shop</a>
+
+<?php include "includes/head.php"; ?>
+
+<body>
+    <?php include "includes/pageheader.php" ?>
+    <div class="home-banner">
+        <div class="banner-text">
+            <h2>SALE</h2>
+            <p>Get massive discounts in our mid-year sale</p>
+            <a href="sale.html">Shop</a>
+        </div>
+    </div>
+    <main class="container">
+        <div class="card">
+            <img src="products/pexels-1.jpg" alt="Nike Dunk Low Retro 'Panda'">
+            <div class="card-content">
+                <h4 class="card-title">Product 1</h4>
+                <p class="price">199</p>
+                <a href="#" class="card-button">
+                    Add to Cart
+                </a>
             </div>
         </div>
-        <main class="container">
-            <div class="card">
-                <img src="products/pexels-1.jpg" alt="Nike Dunk Low Retro 'Panda'">
-                <div class="cart-content">
-                    <h4 class="card-title">Product 1</h4>
-                    <p class="price">199</p>
-                    <a href="#" class="card-button">
-                        Add to Cart
-                    </a>
-                </div>
+        <div class="card">
+            <img src="products/pexels-2.jpg" alt="ASICS Men's Patriot 13 Running Shoes Black">
+            <div class="card-content">
+                <h4 class="card-title">Product 2</h4>
+                <p class="price">199</p>
+                <a href="#" class="card-button">
+                    Add to Cart
+                </a>
             </div>
-            <div class="card">
-                <img src="products/pexels-2.jpg" alt="ASICS Men's Patriot 13 Running Shoes Black">
-                <div class="cart-content">
-                    <h4 class="card-title">Product 2</h4>
-                    <p class="price">199</p>
-                    <a href="#" class="card-button">
-                        Add to Cart
-                    </a>
-                </div>
+        </div>
+        <div class="card">
+            <img src="products/pexels-3.jpg" alt="Nike Vomero 18 Men's Road Running Shoes - Summit White/Coconut Milk/Coconut Milk/Black">
+            <div class="card-content">
+                <h4 class="card-title">Product 3</h4>
+                <p class="price">199</p>
+                <a href="#" class="card-button">
+                    Add to Cart
+                </a>
             </div>
-            <div class="card">
-                <img src="products/pexels-3.jpg" alt="Nike Vomero 18 Men's Road Running Shoes - Summit White/Coconut Milk/Coconut Milk/Black">
-                <div class="cart-content">
-                    <h4 class="card-title">Product 3</h4>
-                    <p class="price">199</p>
-                    <a href="#" class="card-button">
-                        Add to Cart
-                    </a>
-                </div>
+        </div>
+        <div class="card">
+            <img src="products/pexels-4.jpg" alt="Nike ZoomX Vaporfly Next">
+            <div class="card-content">
+                <h4 class="card-title">Product 4</h4>
+                <p class="price">199</p>
+                <a href="#" class="card-button">
+                    Add to Cart
+                </a>
             </div>
-            <div class="card">
-                <img src="products/pexels-4.jpg" alt="Nike ZoomX Vaporfly Next">
-                <div class="cart-content">
-                    <h4 class="card-title">Product 4</h4>
-                    <p class="price">199</p>
-                    <a href="#" class="card-button">
-                        Add to Cart
-                    </a>
-                </div>
+        </div>
+        <div class="card">
+            <img src="products/pexels-5.jpg" alt="The New Balance FuelCell SuperComp Trainer v2 sits sideways on a slab of concrete.">
+            <div class="card-content">
+                <h4 class="card-title">Product 5</h4>
+                <p class="price">199</p>
+                <a href="#" class="card-button">
+                    Add to Cart
+                </a>
             </div>
-            <div class="card">
-                <img src="products/pexels-5.jpg" alt="The New Balance FuelCell SuperComp Trainer v2 sits sideways on a slab of concrete.">
-                <div class="cart-content">
-                    <h4 class="card-title">Product 5</h4>
-                    <p class="price">199</p>
-                    <a href="#" class="card-button">
-                        Add to Cart
-                    </a>
-                </div>
+        </div>
+        <div class="card">
+            <img src="products/pexels-6.jpg" alt="HOKA Torrent 4 Trail Running Shoes">
+            <div class="card-content">
+                <h4 class="card-title">Product 6</h4>
+                <p class="price">199</p>
+                <a href="#" class="card-button">
+                    Add to Cart
+                </a>
             </div>
-            <div class="card">
-                <img src="products/pexels-6.jpg" alt="HOKA Torrent 4 Trail Running Shoes">
-                <div class="cart-content">
-                    <h4 class="card-title">Product 6</h4>
-                    <p class="price">199</p>
-                    <a href="#" class="card-button">
-                        Add to Cart
-                    </a>
-                </div>
+        </div>
+        <div class="bottom-banner">
+            <div class="banner-text">
+                <h2>Clearance Sale</h2>
+                <p>Get massive discounts</p>
+                <a href="clearance.html">Shop Clearance</a>
             </div>
-            <div class="bottom-banner">
-                <div class="banner-text">
-                    <h2>Clearance Sale</h2>
-                    <P>Get massive discounts</P>
-                    <a href="clearance.html">Shop</a>
-                </div>
-            </div>
-
-        </main>
-        <?php include "includes/footer.php"; ?>
-
-    </body>
+        </div>
+    </main>
+    <?php include "includes/footer.php"; ?>
+</body>
 
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
